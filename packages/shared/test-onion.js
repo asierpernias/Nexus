c1f15cfd7d752ee43264e776dad4630793a69fda
APSwinner <ague_soria@yahoo.es>
2026-07-12 23:38:46 +0200

    Text: 3 nodes workflow

--- packages/shared/test-onion.js
+++ packages/shared/test-onion.js
@@ -2,17 +2,27 @@ const { ConstruirCapa, pelarCapa } = require("./onion");
 
 const sodium = require(`libsodium-wrappers`);
 
+
 async function prueba() {
 
     await sodium.ready;
 
-    const clave = sodium.crypto_box_keypair();
+    
+    const clave1 = sodium.crypto_box_keypair();
+    const clave2 = sodium.crypto_box_keypair();
+    const clave3 = sodium.crypto_box_keypair();
 
-    const result1 = await ConstruirCapa("hola mundo", clave.publicKey, "Receptor");
 
-    const resultado = await pelarCapa(result1, clave.privateKey);
+    const result1 = await ConstruirCapa("hola mundo", clave3.publicKey, "Receptor");
+    const result2 = await ConstruirCapa(result1, clave2.publicKey, "B");
+    const result3 = await ConstruirCapa(result2 , clave1.publicKey, "C");
 
-    console.log(resultado)
+    const resultado = await pelarCapa(result3, clave1.privateKey);
+    const resultado2 = await pelarCapa(resultado.payload, clave2.privateKey);
+    const resultado3 = await pelarCapa(resultado2.payload, clave3.privateKey);
+
+
+    console.log(resultado, resultado2, resultado3)
 
 };
 
